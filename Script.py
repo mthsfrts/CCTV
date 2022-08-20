79fd569c25aa8b6b49d0b05404045915c1992c04
matheus <matheus.britto@gmail.com>
2022-08-19 22:26:54 -0300

    Initial Commit

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ import datetime
 
 # Cameras
 
-cap = cv2.VideoCapture("Your RSTP/IP Camera HERE")
+cap = cv2.VideoCapture("Your RSTP/IP Camera HERE!!!!!")
 
 
 # Instances
@@ -18,6 +18,7 @@ SECONDS_TO_RECORD_AFTER_DETECTION = 5
 # methods
 
 def rescale_frame(frame, percent=75):
+    """Method responsible for scale the frame"""
     width = int(frame.shape[1] * percent / 100)
     height = int(frame.shape[0] * percent / 100)
     dim = (width, height)
@@ -25,6 +26,7 @@ def rescale_frame(frame, percent=75):
 
 
 def element(frame):
+    """Method responsible for analyse the frame"""
     face = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     # body = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -35,6 +37,8 @@ def element(frame):
 
 
 def record():
+    """Method responsible for automate the recording of the footage"""
+
     current_time = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -47,12 +51,21 @@ def record():
 
 
 class Cctv:
+    """Class responsible for generate and record the CCTV footage"""
+
     while True:
         _, frame = cap.read()
 
-        scale = rescale_frame(frame, 75)
+        # Image Analysis
+        scale = rescale_frame(frame, 85)
         element(frame)
 
+        # Time Stamp Configuration
+        font = cv2.FONT_HERSHEY_PLAIN
+        cv2.putText(scale, str(datetime.datetime.now()), (1, 15),
+                    font, 1, (255, 255, 230), 2, cv2.LINE_AA)
+
+        # Main Object
         if len(element(frame)) > 0:
             if detection:
                 timer_started = False
@@ -78,7 +91,7 @@ class Cctv:
 
         cv2.imshow("cam_entrance", scale)
 
-        x = cv2.waitKey(1)
+        x = cv2.waitKey(1) & 0xFF
 
         if x == ord("q"):
             break
